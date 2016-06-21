07318dd4391e436efe1b6de833f1351ff2836d69
Erik Douglas <eadouglas10@gmail.com>
2016-06-21 15:10:25 -0400

    Third commit

--- Number_Guessing.rb
+++ Number_Guessing.rb
@@ -1,29 +1,24 @@
 puts "I have a number between 1 and 100. Try to guess what it is!"
 
 secret_number = (rand * 100).to_i + 1
-guess_count = 0
-last_guess = 0
+guesses = []
 answer_found = false
 
-def too_low
-  puts "Your guess was too low. Try again!"
-  guess_count += 1
-end
-
-def too_high
-  puts "Your guess was too high. Try again!"
-  guess_count += 1
-end
-
-while (guess_count < 5 && !answer_found)
-  guess = gets.chomp.to_i
-  if (guess < secret_number)
-    too_low
-  elsif (guess > secret_number)
-    too_high
+def check_guess(guess, answer)
+  if (guess < answer)
+    puts "Your guess was too low.  Try again!"
+  elsif (guess > answer)
+    puts "Your guess was too high.  Try again!"
   else
     puts "You got it!"
-    answer_found = true
   end
-  last_guess = guess
+end
+
+def update()
+
+
+
+while (guesses.length <= 5 && !answer_found)
+  guess = gets.chomp.to_i
+  check_guess(guess, )
 end
