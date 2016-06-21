1046784e2e666d0229003e3936efcca74c8bdc33
Erik Douglas <eadouglas10@gmail.com>
2016-06-21 14:05:53 -0400

    Started definitions for too low and too high

--- Number_Guessing.rb
+++ Number_Guessing.rb
@@ -1,1 +1,15 @@
-puts "I have a number between 1 and 100.  Can you guess what it is?"
+puts "I have a number between 1 and 100. Try to guess what it is!"
+
+secret_number = (rand * 100).to_i + 1
+incorrect_count = 0
+last guess = 0
+
+guess = gets.chomp.to_i
+
+def too_low
+  puts "Your guess was too low. Try again!"
+  incorrect_count += 1
+end
+
+def too_high
+  puts "Your guess was too high. Try again!"
