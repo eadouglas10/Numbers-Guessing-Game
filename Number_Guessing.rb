ba313f38c279ccb593b02cb4c2c3e2bdaabff2b3
Erik Douglas <eadouglas10@gmail.com>
2016-06-21 16:27:36 -0400

    Got it pretty much working in hard mode but still cleaning it up.

--- Number_Guessing.rb
+++ Number_Guessing.rb
@@ -1,24 +1,52 @@
 puts "I have a number between 1 and 100. Try to guess what it is!"
 
-secret_number = (rand * 100).to_i + 1
+secret_number = rand(1..100)
 guesses = []
 answer_found = false
+guess_higher_next_time = false
 
-def check_guess(guess, answer)
-  if (guess < answer)
-    puts "Your guess was too low.  Try again!"
-  elsif (guess > answer)
-    puts "Your guess was too high.  Try again!"
-  else
-    puts "You got it!"
-  end
+def too_high
+  puts "Your guess was too high. Try again!"
 end
 
-def update()
+def too_low
+  puts "Your guess was too low. Try again!"
+end
+
+def bad_strategy(guesses_so_far, higher)
+  len = guesses_so_far.length
+  if (len > 1)
+    for guess in 0...(len - 1)
+      if guesses_so_far[guess] == guesses_so_far.last
+        return true
+      end
+    end
+  end
+  if (guesses_so_far.last > guesses_so_far[len - 2] && !higher)
+    return true
+  end
+  if (guesses_so_far.last < guesses_so_far[len - 2] && higher)
+    return true
+  end
+  return false
+end
 
 
+while (guesses.length < 5 && !answer_found) do
+  guesses.push(gets.chomp.to_i)
 
-while (guesses.length <= 5 && !answer_found)
-  guess = gets.chomp.to_i
-  check_guess(guess, )
+  if bad_strategy(guesses, guess_higher_next_time)
+    puts "Are you sure you know what you're doing?"
+  end
+
+  if guesses.last < secret_number
+    too_low
+    guess_higher_next_time = true
+  elsif guesses.last > secret_number
+    too_high
+    guess_higher_next_time = false
+  else
+    puts "You got it!"
+    answer_found = true
+  end
 end
